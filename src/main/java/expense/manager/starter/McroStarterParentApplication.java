8431e57b29a5b5f449a395c444ab7208240db561
cdkakade <cdkakade1@gmail.com>
2024-05-24 15:34:31 +0530

    Initial commit

---
+++ src/main/java/expense/manager/starter/McroStarterParentApplication.java
@@ -0,0 +1,13 @@
+package expense.manager.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McroStarterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McroStarterParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expense/manager/starter/McroStarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package expense.manager.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McroStarterParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
